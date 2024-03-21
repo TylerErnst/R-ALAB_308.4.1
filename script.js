0b93d112ba39f980ae0e21e6f285a32c5fea8200
TylerErnst <tcernst5@gmail.com>
2024-03-21 09:02:38 -0600

    Part 1 and cleanup

--- script.js
+++ script.js
@@ -11,36 +11,9 @@
 
 
 
-/*
+
 
 // 308.3.1 Part 3: Feeling Loopy
-console.log('\n\n\n\nPart 3:\n\n');
-
-// As a final task, solve the following practical problem regarding string processing.
-// Context: A CSV file, or “Comma-Separated Values” file is traditionally used to store tabular data.
-// You may be familiar with CSVs through past use of programs such as Microsoft Excel or Google Sheets.
-// While each of these programs save their data in different formats to preserve style
-// (e.g., font color or cell backgrounds), at their core, they are storing CSV data.
-
-// CSV data looks like this:
-// ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
-// Not very nice to look at. The “\n” is an escaped Line Feed, which indicates that the following data should
-// begin on a new line, as follows:
-// ID,Name,Occupation,Age
-// 42,Bruce,Knight,41
-// 57,Bob,Fry Cook,19
-// 63,Blaine,Quiz Master,58
-// 98,Bill,Doctor’s Assistant,26
-// As you may have guessed, these values being “comma-separated” means that each comma is also a delimiter.
-// This is why this type of data is traditionally viewed in tables. Here is how the data looks once fully parsed:
-// ID	Name	Occupation	Age
-// 42	Bruce	Knight	41
-// 57	Bob	Fry Cook	19
-// 63	Blaine	Quiz Master	58
-// 98	Bill	Doctor’s Assistant	26
-
-// Your task is to write a script that accomplishes the following:
-// Loop through the characters of a given CSV string.
 let str = 'ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26'
 let cell = [];
 let row= [];
@@ -68,110 +41,6 @@ for (const i in str){
     }
     
 }  
-// Log each row of data.
-// You do not need to format the data, the following works well.
-// console.log(cell1, cell2, cell3, cell4);
-// You can make the following assumptions:
-// There will only be 4 cells per row.
-// There will be no escaped characters other than “\n”.
-
-
-
-
-
-
-//Use the example string provided above to test your program. Once you are confident it is working correctly,
-// try the following string to see if your program works properly with other data.
-console.log('\nExtra\n')
-
-str = 'Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232'
-cell = [];
-row= [];
-cellIndicator = 0;
-for (const i in str){
-// Store each “cell” of data in a variable.
-
-// When you encounter a comma, move to the next cell.
-    if (str[i] === ','){
-        cell[cellIndicator] = cell[cellIndicator] + '\t';
-        cellIndicator++;
-        continue;
-    }
-// When you encounter the “\n” sequence, move to the next “row.”
-    if (str[i] === '\n'){
-        console.log(cell[0],cell[1],cell[2],cell[3]);
-        cellIndicator=0;
-        cell = [];
-        continue;
-    }
-    if (cell[cellIndicator]){
-    cell[cellIndicator] = cell[cellIndicator] + str[i];
-    }else{
-        cell[cellIndicator] = str[i];
-    }
-    
-} 
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// PART 3: FEELING LOOPY
-
-
-// for (i = 0; i < csv.length; i++) {
-//   // ADDS THE LETTER TO THE WORD(CELL)
-//   if (csv[i] != ',') {
-//     cell += csv[i];
-//   }
-
-//   if (csv[i] === ',') {
-//     // ONCE IT ENCOUNTERS A COMMA IT ADDS THE WORD(CELL) TO THE ROW
-//     // THEN IT CLEARS THE CELL VARIABLE SO IT CAN BUILD THE NEXT WORD(CELL)
-//     // console.log('CELL ', cell);
-//     row += `${cell} `;
-//     cell = '';
-//   }
-
-//   if (csv[i] === '\n') {
-//     // ONCE IT ENCOUNTERS A LINE BREAK, IT ADDS THE FINAL WORD(CELL) TO THE ROW AND PRINTS THE ROW
-//     // THEN IT CLEARS THE ROW AND CELL
-//     row += `${cell}`;
-//     console.log(row);
-//     cell = '';
-//     row = '';
-//   }
-
-//   if (i === csv.length - 1) {
-//     // PRINTS THE LAST ROW
-//     row += `${cell}`;
-//     console.log(row);
-//   }
-// }
-
-// **************
-// let table1 = testData.split('\n');
-// table1.forEach(element => {
-//     console.log(element)
-//     let row = element.split(',');
-//     console.log(row[1]);
-// });
-// ****************
-
-
 
 
 
@@ -264,3 +133,85 @@ people.forEach(person => {
 console.log(convertToCSV.slice(0, -2));
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// misc code
+
+
+// PART 3: FEELING LOOPY
+
+
+// for (i = 0; i < csv.length; i++) {
+//   // ADDS THE LETTER TO THE WORD(CELL)
+//   if (csv[i] != ',') {
+//     cell += csv[i];
+//   }
+
+//   if (csv[i] === ',') {
+//     // ONCE IT ENCOUNTERS A COMMA IT ADDS THE WORD(CELL) TO THE ROW
+//     // THEN IT CLEARS THE CELL VARIABLE SO IT CAN BUILD THE NEXT WORD(CELL)
+//     // console.log('CELL ', cell);
+//     row += `${cell} `;
+//     cell = '';
+//   }
+
+//   if (csv[i] === '\n') {
+//     // ONCE IT ENCOUNTERS A LINE BREAK, IT ADDS THE FINAL WORD(CELL) TO THE ROW AND PRINTS THE ROW
+//     // THEN IT CLEARS THE ROW AND CELL
+//     row += `${cell}`;
+//     console.log(row);
+//     cell = '';
+//     row = '';
+//   }
+
+//   if (i === csv.length - 1) {
+//     // PRINTS THE LAST ROW
+//     row += `${cell}`;
+//     console.log(row);
+//   }
+// }
+
+// **************
+// let table1 = testData.split('\n');
+// table1.forEach(element => {
+//     console.log(element)
+//     let row = element.split(',');
+//     console.log(row[1]);
+// });
+// ****************
